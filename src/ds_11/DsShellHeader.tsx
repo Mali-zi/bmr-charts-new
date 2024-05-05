d87d8aba0a1a42c3ebb9ddb58d37d6239bed2de4
Natali Zikrach <mne310872@yandex.ru>
2024-05-05 22:58:49 +0300

    feat: add Consta

--- src/ds_11/DsShellHeader.tsx
+++ src/ds_11/DsShellHeader.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from "react";
 import { Breadcrumbs } from "@consta/uikit/Breadcrumbs";
 import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import {L10n} from 'bi-internal/ui';
 
 interface IDsShellHeaderProps {
   schema_name: string;
@@ -10,9 +11,27 @@ interface IDsShellHeaderProps {
 }
 
 const CBreadcrumbs = () => {
+  const pages = [
+    {
+      label: <L10n>datasets</L10n>,
+      href: '/#/ds',
+    },
+    {
+      label: 'Страница 1',
+      href: 'https://url.com/page-1',
+    },
+    {
+      label: 'Страница 2',
+      href: 'https://url.com/page-2',
+    },
+    {
+      label: 'Страница 3',
+      href: 'https://url.com/page-3',
+    },
+  ];
   return (
     <div className="CBreadcrumbs">
-      {/* <Breadcrumbs items={pages} size="xs" onlyIconRoot /> */}
+      <Breadcrumbs items={pages} size="xs" onlyIconRoot />
     </div>
   );
 };
